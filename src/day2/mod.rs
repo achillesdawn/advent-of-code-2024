f425f654c8481fb1f3e1df07dc9bf4e4e0137308
miguel <vackap@gmail.com>
2024-12-02 15:22:53 -0300

    day 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -8,6 +8,7 @@ fn read_input() -> String {
     file
 }
 
+#[allow(unused)]
 fn process_line(line: &str) -> bool {
     let split = line.split(" ");
 
@@ -38,6 +39,16 @@ fn process_line(line: &str) -> bool {
     true
 }
 
+#[allow(unused)]
+pub fn problem1() {
+    let s = read_input();
+
+    let lines = s.lines();
+
+    let result: usize = lines.map(process_line).filter(|i| *i).count();
+    dbg!(result);
+}
+
 fn check_abs(item: i16, last: i16) -> bool {
     let diff = item - last;
 
@@ -50,19 +61,25 @@ fn check(item: i16, last: i16, incrementing: bool) -> bool {
 
     let abs_diff = diff.abs();
 
-    // dbg!(diff, abs_diff, diff > 0, (diff > 0) == incrementing, (1..=3).contains(&abs_diff));
+    dbg!(
+        diff,
+        abs_diff,
+        diff > 0,
+        (diff > 0) == incrementing,
+        (1..=3).contains(&abs_diff)
+    );
     ((diff > 0) == incrementing) && (1..=3).contains(&abs_diff)
 }
 
-fn process_line_problem_2(line: &str, missed: bool) -> bool {
+fn line_to_num(line: &str) -> Vec<i16> {
     let split = line.split(" ");
 
-    let split: Vec<i16> = split.map(|item| item.parse::<i16>().unwrap()).collect();
+    split.map(|item| item.parse::<i16>().unwrap()).collect()
+}
 
+fn process_line_problem_2(split: Vec<i16>, missed: bool) -> bool {
     let mut last = 0i16;
-    let mut incrementing = (split[1] - split[0]) > 0;
-
-    let mut missed = false;
+    let incrementing = (split[1] - split[0]) > 0;
 
     for (idx, current) in split.iter().enumerate() {
         if idx == 0 {
@@ -70,90 +87,63 @@ fn process_line_problem_2(line: &str, missed: bool) -> bool {
             continue;
         }
 
-        let safe = check(*current, last, incrementing);
+        let diff = current - last;
 
-        if safe {
-        } else {
-            dbg!(line, idx, current, last, incrementing);
+        let abs_diff = diff.abs();
 
+        let safe = ((diff > 0) == incrementing) && (1..=3).contains(&abs_diff);
+
+        if !safe {
             if !missed {
-                missed = true;
+                let without_current: Vec<i16> = split
+                    .iter()
+                    .enumerate()
+                    .filter(|(id, _)| *id != idx)
+                    .map(|(_, i)| *i)
+                    .collect();
 
-                if idx == 1 {
-                    let next = split[idx + 1];
+                let without_current = process_line_problem_2(without_current, true);
 
-                    let last_safe = check_abs(next, last);
-
-                    let current_safe = check_abs(next, *current);
-
-                    if last_safe {
-                        println!("removing idx {} item {}", idx, current);
-                        println!("-----------");
-                        incrementing = (next - last) > 0;
-                        continue;
-                    } else if current_safe {
-                        println!("removing idx {} item {}", idx - 1, last);
-                        println!("-----------");
-                        last = *current;
-
-                        incrementing = (next - current) > 0;
-                        continue;
-                    } else {
-                        println!("None safe");
-                        println!("-----------");
-                        return false;
-                    }
-                } else if idx == 2 {
-                    let previous = split[idx - 2];
-
-                    let last_safe = check_abs(previous, last);
-                    let previous_safe = check_abs(previous, *current);
-
-                    if last_safe {
-                        println!("removing idx {} item {}", idx-1, last);
-                        println!("-----------");
-                        incrementing = (next - last) > 0;
-                        continue;
-                    } else if previous_safe {
-                        println!("removing idx {} item {}", idx - 1, last);
-                        println!("-----------");
-                        last = *current;
-
-                        incrementing = (next - current) > 0;
-                        continue;
-                    } else {
-                        println!("None safe");
-                        println!("-----------");
-                        return false;
-                    }
-                } else {
-
+                if without_current {
+                    return true;
                 }
 
-                continue;
+                let without_last: Vec<i16> = split
+                    .iter()
+                    .enumerate()
+                    .filter(|(id, _)| *id != (idx - 1))
+                    .map(|(_, i)| *i)
+                    .collect();
+
+                let without_last = process_line_problem_2(without_last, true);
+
+                if without_last {
+                    return true;
+                }
+
+                if idx >= 2 {
+                    let without_previous: Vec<i16> = split
+                        .iter()
+                        .enumerate()
+                        .filter(|(id, _)| *id != (idx - 2))
+                        .map(|(_, i)| *i)
+                        .collect();
+
+                    let without_previous = process_line_problem_2(without_previous, true);
+
+                    if without_previous {
+                        return true;
+                    }
+                }
             }
-            println!("FAIL");
-            println!("-----------");
+
             return false;
         }
 
         last = *current;
     }
-    if missed {
-        println!("PASS");
-        println!("-----------");
-    }
 
     true
-}
-
-pub fn problem1() {
-    let s = read_input();
-
-    let lines = s.lines();
-
-    let result: usize = lines.map(process_line).filter(|i| *i).count();
-    dbg!(result);
 }
 
 pub fn problem2() {
@@ -161,87 +151,99 @@ pub fn problem2() {
 
     let lines = s.lines();
 
-    let result: usize = lines.map(process_line_problem_2).filter(|i| *i).count();
+    let result: usize = lines
+        .map(line_to_num)
+        .map(|i| process_line_problem_2(i, false))
+        .filter(|i| *i)
+        .count();
+
     dbg!(result);
 }
 
 #[cfg(test)]
 mod test {
+    use crate::day2::line_to_num;
 
-    #[test]
-    fn test_case_one() {
-        let s = "8 11 14 16 15";
-        let safe = super::process_line(s);
-        assert_eq!(safe, false);
+    use super::{process_line, process_line_problem_2};
 
-        let s = "8 11 14 16 17";
-        let safe = super::process_line(s);
-        assert_eq!(safe, true);
-    }
+    // #[test]
+    // fn test_case_one() {
+    //     let s = "8 11 14 16 15";
+    //     let safe = super::process_line(s);
+    //     assert_eq!(safe, false);
+
+    //     let s = "8 11 14 16 17";
+    //     let safe = super::process_line(s);
+    //     assert_eq!(safe, true);
+    // }
 
     #[test]
     fn test_case_one_two() {
         let s = "8 8 14 16 15";
-        let safe = super::process_line(s);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, false);
 
         let s = "8 1 14 16 17";
-        let safe = super::process_line(s);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, false);
 
         let s = "8 9 14 16 17";
-        let safe = super::process_line(s);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, false);
 
         let s = "8 11 14 16 17";
-        let safe = super::process_line(s);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "8 7 8 16 17";
-        let safe = super::process_line(s);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, false);
     }
 
     #[test]
     fn test_problem_two_one() {
         let s = "7 6 4 2 1";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "1 2 7 8 9";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, false);
 
         let s = "9 7 6 2 1";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, false);
 
         let s = "1 3 2 4 5";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "8 6 4 4 1";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "1 3 6 7 9";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "63 67 70 72 74 75 78 79";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "67 70 72 74 75 78 79 99";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "67 68 1 71 74 75 78 79";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
 
         let s = "46 43 47 48 49 50";
-        let safe = super::process_line_problem_2(s, false);
+        let safe = process_line_problem_2(line_to_num(s), false);
+        assert_eq!(safe, true);
+
+        let s = "44 47 48 49 50 54";
+        let safe = process_line_problem_2(line_to_num(s), false);
         assert_eq!(safe, true);
     }
 }
