6efb450eece5f619bbae2f83ff84d24f26069cf8
miguel <vackap@gmail.com>
2024-12-04 23:33:04 -0300

    diagonals

--- src/day4/grid.rs
+++ src/day4/grid.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum Direction {
     Up,
     Down,
--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -16,10 +16,65 @@ use std::{
 
 use grid::{Direction, Grid};
 
-struct XmasCheck {
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Diagonal {
     x: usize,
     y: usize,
     direction: Direction,
+}
+
+fn diagonals(x: usize, y: usize, direction: &Direction) -> (Diagonal, Diagonal) {
+    match direction {
+        Direction::UpRight => (
+            Diagonal {
+                x: x + 2,
+                y,
+                direction: Direction::UpLeft,
+            },
+            (Diagonal {
+                x,
+                y: y - 2,
+                direction: Direction::DownRight,
+            }),
+        ),
+        Direction::UpLeft => (
+            Diagonal {
+                x: x - 2,
+                y,
+                direction: Direction::UpRight,
+            },
+            (Diagonal {
+                x,
+                y: y - 2,
+                direction: Direction::DownLeft,
+            }),
+        ),
+        Direction::DownRight => (
+            Diagonal {
+                x,
+                y: y + 2,
+                direction: Direction::UpRight,
+            },
+            (Diagonal {
+                x: x + 2,
+                y,
+                direction: Direction::DownLeft,
+            }),
+        ),
+        Direction::DownLeft => (
+            Diagonal {
+                x,
+                y: y + 2,
+                direction: Direction::UpRight,
+            },
+            (Diagonal {
+                x: x + 2,
+                y,
+                direction: Direction::DownLeft,
+            }),
+        ),
+        _ => panic!(),
+    }
 }
 
 fn search(mut g: Grid) -> u32 {
@@ -31,6 +86,7 @@ fn search(mut g: Grid) -> u32 {
     ];
 
     let mut count = 0u32;
+    let mut checked = HashSet::new();
 
     for y in 0..g.cols {
         for x in 0..g.rows {
@@ -42,65 +98,69 @@ fn search(mut g: Grid) -> u32 {
             for direction in directions.iter() {
                 let result = g.check_direction(x, y, direction);
                 if result {
-                    // println!("FOUND AT {}, {} direction: {}", x, y, direction);
-                    let (check1, check2) = match direction {
-                        Direction::UpRight => (
-                            XmasCheck {
-                                x: x + 2,
-                                y,
-                                direction: Direction::UpLeft,
-                            },
-                            (XmasCheck {
-                                x,
-                                y: y - 2,
-                                direction: Direction::DownRight,
-                            }),
-                        ),
-                        Direction::UpLeft => (
-                            XmasCheck {
-                                x: x - 2,
-                                y,
-                                direction: Direction::UpRight,
-                            },
-                            (XmasCheck {
-                                x,
-                                y: y - 2,
-                                direction: Direction::DownLeft,
-                            }),
-                        ),
-                        Direction::DownRight => (
-                            XmasCheck {
-                                x,
-                                y: y + 2,
-                                direction: Direction::UpRight,
-                            },
-                            (XmasCheck {
-                                x: x + 2,
-                                y,
-                                direction: Direction::DownLeft,
-                            }),
-                        ),
-                        Direction::DownLeft => (
-                            XmasCheck {
-                                x,
-                                y: y + 2,
-                                direction: Direction::UpRight,
-                            },
-                            (XmasCheck {
-                                x: x + 2,
-                                y,
-                                direction: Direction::DownLeft,
-                            }),
-                        ),
-                        _ => panic!(),
+                    let first_check = Diagonal {
+                        x,
+                        y,
+                        direction: direction.clone(),
                     };
 
-                    let check = g.check_direction(x, y, new_direction1);
-                    if check {
-                        println!(
-                            "Found X-MAS, x: {}, y: {}, direction: {} {}",
-                            x, y, direction, new_direction1
+                    if checked.contains(&first_check) {
+                        continue;
+                    }
+
+                    let (first_diagonal, second_diagonal) = diagonals(x, y, direction);
+
+                    let mut checked_result;
+                    if checked.contains(&first_diagonal) {
+                        checked_result = false;
+                    } else {
+                        checked_result = g.check_direction(
+                            first_diagonal.x,
+                            first_diagonal.y,
+                            &first_diagonal.direction,
                         );
+                    }
+
+                    if checked_result {
+                        println!(
+                            "Found X-MAS, x: {}, y: {}, direction: {} and x:{} y:{} direction: {}",
+                            x,
+                            y,
+                            direction,
+                            first_diagonal.x,
+                            first_diagonal.y,
+                            first_diagonal.direction
+                        );
+                        checked.insert(first_check);
+                        checked.insert(first_diagonal);
+
+                        count += 1;
+                        continue;
+                    }
+
+                    if checked.contains(&second_diagonal) {
+                        checked_result = false;
+                    } else {
+                        checked_result = g.check_direction(
+                            second_diagonal.x,
+                            second_diagonal.y,
+                            &second_diagonal.direction,
+                        );
+                    }
+
+                    if checked_result {
+                        println!(
+                            "Found X-MAS, x: {}, y: {}, direction: {} and x:{} y:{} direction: {}",
+                            x,
+                            y,
+                            direction,
+                            second_diagonal.x,
+                            second_diagonal.y,
+                            second_diagonal.direction
+                        );
+                        checked.insert(first_check);
+                        checked.insert(second_diagonal);
+
                         count += 1;
                     }
                 }
